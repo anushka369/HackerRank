6371610d6a8bbcfbd758937274144926eac539ce
anushka <81908360+anushka369@users.noreply.github.com>
2025-01-19 11:59:16 +0530

    Create Introducing Arithmetic Operators.js

---
+++ JavaScript/Introducing Arithmetic Operators.js
@@ -0,0 +1,8 @@
+var add = a + b;
+var sub = a - b;
+var mul = a * b;
+var div = a / b;
+var inc = ++a;
+var dec = --b;
+
+// Link to the problem: https://www.hackerrank.com/contests/7days-javascript/challenges/js-arithmetic-operators/
