d33e92b0aab7fc42597b2e5f2d9d7d1d02c75d24
anushka <81908360+anushka369@users.noreply.github.com>
2025-01-20 22:13:12 +0530

    Create Valid Username Regular Expression.java

---
+++ Java/Valid Username Regular Expression.java
@@ -0,0 +1,6 @@
+class UsernameValidator 
+{
+      public static final String regularExpression = "^[a-zA-Z][\\w]{7,29}$";
+}
+
+// Link to the problem: https://www.hackerrank.com/challenges/valid-username-checker/problem?isFullScreen=true/
