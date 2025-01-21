4aee89fbe16c80e65dbb13bb43e5283a2ea64a7c
anushka <81908360+anushka369@users.noreply.github.com>
2025-01-21 19:49:22 +0530

    Create Java Regex.java

---
+++ Java/Java Regex.java
@@ -0,0 +1,7 @@
+class MyRegex 
+{
+    String num = "([01]?\\d{1,2}|2[0-4]\\d|25[0-5])";
+    String pattern = num + "." +  num + "." +  num + "." + num;
+}
+
+// Link to the problem: https://www.hackerrank.com/challenges/java-regex/problem?isFullScreen=true/
