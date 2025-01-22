c2fdecf486d615aacae1bec2c05f4f421d904c32
anushka <81908360+anushka369@users.noreply.github.com>
2025-01-22 10:07:17 +0530

    Create Java Inheritance I.java

---
+++ Java/Java Inheritance I.java
@@ -0,0 +1,14 @@
+class Bird extends Animal
+{
+    void fly()
+    {
+        System.out.println("I am flying");
+    }
+    
+    void sing()
+    {
+        System.out.println("I am singing");
+    }
+}
+
+// Link to the problem: https://www.hackerrank.com/challenges/java-inheritance-1/problem?isFullScreen=true/
