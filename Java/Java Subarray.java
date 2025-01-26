e84bf0e4f09c587a3dff1be1a71e1e1fb055e81e
anushka <81908360+anushka369@users.noreply.github.com>
2025-01-27 02:33:27 +0530

    Create Java Subarray.java

---
+++ Java/Java Subarray.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution 
+{
+    public static void main(String[] args) 
+    {
+       Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[][] A = new int[2][n + 1];
+
+        for (int i = 1; i <= n; i++) 
+        {
+            A[0][i] = scan.nextInt();
+            A[1][i] = A[1][i - 1] + A[0][i];
+        }
+        int res = 0;
+
+        for (int i = 0; i <= n; i++)
+        {
+            if (A[0][i] < 0)
+            {
+                res++;
+            }
+        }
+
+        for (int i = 2; i <= n; i++)
+        {    
+            for (int j = 0, k = j + i; k <= n; j++, k++)
+            {
+                if (A[1][k] - A[1][j] < 0)
+                {
+                    res++;
+                }
+            }
+        }
+
+        System.out.println(res);
+    }
+}
+
+// Link to the problem: https://www.hackerrank.com/challenges/java-negative-subarray/problem?isFullScreen=true/
