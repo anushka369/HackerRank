6d70d3fac88a339ca39c1a1aa9af652d1a47aad4
anushka <81908360+anushka369@users.noreply.github.com>
2025-01-27 02:51:14 +0530

    Create Java Stack.java

---
+++ Java/Java Stack.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+class Solution
+{   
+    public static void main(String []argh)
+    {
+        Scanner sc = new Scanner(System.in);
+       
+        while (sc.hasNext()) 
+        {
+            String input = sc.next();
+            Stack <Character> stack = new Stack();
+          
+            for (int i = 0; i < input.length(); i++)
+            {
+                if (stack.isEmpty() == false)
+                {
+                    switch(input.charAt(i))
+                    {
+                        case '}':
+                            if (stack.peek() == '{')
+                            {
+                                stack.pop();
+                            }
+                            break;
+                        
+                        case ']': 
+                            if (stack.peek() == '[')
+                            {
+                                stack.pop();
+                            }
+                            break;
+                        
+                        case ')': 
+                            if (stack.peek() == '(')
+                            {
+                                stack.pop();
+                            }
+                            break;
+                        
+                        default:
+                            stack.push(input.charAt(i));
+                    }
+                }
+            
+                else
+                {
+                    stack.push(input.charAt(i));
+                }         
+            }    
+            System.out.println(stack.isEmpty());               
+        }       
+    }   
+}
+
+// Link to the problem: https://www.hackerrank.com/challenges/java-stack/problem?isFullScreen=true/
