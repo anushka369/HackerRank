816bc6e64667a1ea91332655c20668611ce40dcc
anushka <81908360+anushka369@users.noreply.github.com>
2025-01-31 22:05:02 +0530

    Create Day 1: Functions.js

---
+++ JavaScript/10 Days of JavaScript/Day 1: Functions.js
@@ -0,0 +1,46 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => 
+{
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => 
+{
+    inputString = inputString.trim().split('\n').map(string => 
+    {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() 
+{
+    return inputString[currentLine++];
+}
+
+function factorial(n) 
+{
+    if (n < 0) 
+    {
+        return undefined;
+    }
+    
+    let result = 1;
+  
+    for (let i = 1; i <= n; i++) 
+    {
+        result *= i;
+    }
+  
+    return result;
+}
+
+// Link to the problem: https://www.hackerrank.com/challenges/js10-function/problem?isFullScreen=true/
