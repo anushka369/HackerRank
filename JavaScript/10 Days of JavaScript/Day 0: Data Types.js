12093de85cfcb21ee5d44b47c1a60035a67359bf
anushka <81908360+anushka369@users.noreply.github.com>
2025-01-31 22:13:32 +0530

    Update Day 0: Data Types.js

--- JavaScript/10 Days of JavaScript/Day 0: Data Types.js
+++ JavaScript/10 Days of JavaScript/Day 0: Data Types.js
@@ -27,8 +27,7 @@ function readLine()
 }
 
 function performOperation(secondInteger, secondDecimal, secondString) 
-{
-    
+{    
     const firstInteger = 4;    
     const firstDecimal = 4.0;    
     const firstString = 'HackerRank ';
@@ -36,6 +35,15 @@ function performOperation(secondInteger, secondDecimal, secondString)
     console.log(Number(firstInteger) + Number(secondInteger));    
     console.log(Number(firstDecimal) + Number(secondDecimal));    
     console.log(firstString+secondString);
+}
+
+function main() 
+{
+    const secondInteger = readLine();
+    const secondDecimal = readLine();
+    const secondString = readLine();
+    
+    performOperation(secondInteger, secondDecimal, secondString);
 }
 
 // Link to the problem: https://www.hackerrank.com/challenges/js10-data-types/problem?isFullScreen=true/
