72058eba273d8fab48f0209b439347b6ac5f047c
anushka <81908360+anushka369@users.noreply.github.com>
2025-02-13 17:23:36 +0530

    Update Day 3: Sort Array of Objects.js

--- JavaScript/7 Days of JavaScript/Day 3: Sort Array of Objects.js
+++ JavaScript/7 Days of JavaScript/Day 3: Sort Array of Objects.js
@@ -1,12 +1,34 @@
 function sortLibrary() 
-{
-    library.sort(function(a,b) 
+{  
+    libraryLength = library.length;
+
+    if (libraryLength <= 1) 
     {
-        return a.title.localeCompare(b.title);
-    });
-  
-    console.log(library);
-} 
+        return library;
+    }
+    
+    sorted = false;
+
+    while (!sorted) 
+    {
+        sorted = true;
+
+        for (var i = 0; i < libraryLength-1; i++) 
+        {
+            if (library[i].title > library[i+1].title) 
+            {
+                tmp = library[i];
+                library[i] = library[i+1];
+                library[i+1] = tmp;
+                sorted = false;
+            }
+        }
+    }
+    
+    console.log("[ { author: '"+library[0].author+"',\n    title: '"+library[0].title+"',\n    libraryID: "+library[0].libraryID+" },");
+    console.log("  { author: '"+library[1].author+"',\n    title: '"+library[1].title+"',\n    libraryID: "+library[1].libraryID+" },");
+    console.log("  { author: '"+library[2].author+"',\n    title: '"+library[2].title+"',\n    libraryID: "+library[2].libraryID+" } ]");
+}
 
 // tail starts here
 var library = [
