8dea0ebc50c27649f2f34c1f7eca1357bf773d3f
anushka <81908360+anushka369@users.noreply.github.com>
2025-02-13 17:26:38 +0530

    Update Day 6: Mark and his Toys.js

--- JavaScript/7 Days of JavaScript/Day 6: Mark and his Toys.js
+++ JavaScript/7 Days of JavaScript/Day 6: Mark and his Toys.js
@@ -1,30 +1,24 @@
-"use strict";
 function processData(input) 
 {
-    let input_array = input.split('\n');
-    let money = parseInt(input_array[0].split(' ')[1]);
-    let toys_prices_str = input_array[1].split(' ');
-    let toys_prices = toys_prices_str.map(price_str => parseInt(price_str));
-    toys_prices.sort((a,b) => a - b);
-    let number_of_toys = 0;
+    var rows = input.split('\n');
+    var max = rows[0].split(' ').map(Number)[1];
+    var arr = rows[1].split(' ').map(Number);
+    arr = arr.sort(function(a, b){return a-b}); 
+    var i = 0;
     
-    for(let price of toys_prices) 
+    while (max >= 0) 
     {
-        money = money - price;
-        if (money <= 0) 
-        {
-            break;
-        }
-        
-        number_of_toys++;
+        max -= arr[i];
+        i++;
     }
     
-    console.log(number_of_toys);
-} 
+    console.log(i-1);
+}
 
 process.stdin.resume();
 process.stdin.setEncoding("ascii");
 let _input = "";
+
 process.stdin.on("data", function (input) 
 {
     _input += input;
