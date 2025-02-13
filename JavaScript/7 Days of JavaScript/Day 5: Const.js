cfdb2ebcdff7d68ae428dc83871ba4ff78262fd2
anushka <81908360+anushka369@users.noreply.github.com>
2025-02-13 17:17:41 +0530

    Update Day 5: Const.js

--- JavaScript/7 Days of JavaScript/Day 5: Const.js
+++ JavaScript/7 Days of JavaScript/Day 5: Const.js
@@ -2,4 +2,9 @@ const PI = Math.PI;
 const EULER = Math.E;
 const SQRT2 = Math.SQRT2;
 
+console.log(EULER);
+console.log(PI);
+console.log(SQRT2);
+return 0;
+
 // Link to the problem: https://www.hackerrank.com/contests/7days-javascript/challenges/js-const/
