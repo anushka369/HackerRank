dc1f72cfa26656b1ce84a62c8782a8a0f257acd7
anushka <81908360+anushka369@users.noreply.github.com>
2025-02-13 17:05:16 +0530

    Update Day 5: Let.js

--- JavaScript/7 Days of JavaScript/Day 5: Let.js
+++ JavaScript/7 Days of JavaScript/Day 5: Let.js
@@ -7,4 +7,4 @@ for (let index = 0; index < N; index++)
 
 console.log("The global index (after for-loop) is : ",index);
 
-// Link to the problem: https://www.hackerrank.com/contests/7days-javascript/challenges/js-let
+// Link to the problem: https://www.hackerrank.com/contests/7days-javascript/challenges/js-let/
