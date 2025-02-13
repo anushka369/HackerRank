cd13b08a78a89943db828194dd5673a1ff077423
anushka <81908360+anushka369@users.noreply.github.com>
2025-02-13 16:57:21 +0530

    Create Day 5: Let.js

---
+++ JavaScript/7 Days of JavaScript/Day 5: Let.js
@@ -0,0 +1,10 @@
+console.log("The global index (before for-loop) is : ", index);
+
+for (let index = 0; index < N; index++)
+{
+    console.log("The local index is : ",index);
+}
+
+console.log("The global index (after for-loop) is : ",index);
+
+// Link to the problem: https://www.hackerrank.com/contests/7days-javascript/challenges/js-let
