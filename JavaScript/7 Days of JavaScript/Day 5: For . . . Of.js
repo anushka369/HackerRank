43601db531726dc07b8f3648e734768785f6e0a1
anushka <81908360+anushka369@users.noreply.github.com>
2025-02-13 17:00:57 +0530

    Create Day 5: For . . . Of.js

---
+++ JavaScript/7 Days of JavaScript/Day 5: For . . . Of.js
@@ -0,0 +1,10 @@
+'use strict'
+for (let s of my_array)
+{
+    if (s === s.split('').reverse().join('')) 
+    {
+        console.log(s);
+    }
+}
+
+// Link to the problem: https://www.hackerrank.com/contests/7days-javascript/challenges/js-for-of
