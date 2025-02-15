a523f7931844f77d691c862c4b152d392df64d94
anushka <81908360+anushka369@users.noreply.github.com>
2025-02-15 20:07:37 +0530

    Create Day 8: Dictionaries and Maps.py

---
+++ Tutorials/30 Days of Code/Day 8: Dictionaries and Maps.py
@@ -0,0 +1,20 @@
+n = int(input())
+phonebook = dict()
+
+for i in range(n):
+    line = input()
+    line = line.split()
+    phonebook[line[0]] = phonebook.get(line[0],line[1])
+
+while 1:
+    try:
+        q = input()
+        
+        if q in phonebook:
+            print(str(q) + "=" + str(phonebook[q]))
+        else:
+            print("Not found")
+    except:
+        break
+
+# Link to the problem: https://www.hackerrank.com/challenges/30-dictionaries-and-maps/problem?isFullScreen=true/
