e44f914b229bd980f28bc028db697d9094442a54
anushka <81908360+anushka369@users.noreply.github.com>
2025-02-15 20:39:56 +0530

    Added to folder

--- Tutorials/30 Days of Code/Day 00: Hello, World.c
+++ Tutorials/30 Days of Code/Day 00: Hello, World.c
@@ -12,7 +12,7 @@ int main()
     scanf("%[^\n]", input_string); 
     
     // Print a string literal saying "Hello, World." to stdout using printf.
-    printf("Hello, World.\n");
+    printf("Hello, World. \n");
     
     // TODO: Write a line of code here that prints the contents of input_string to stdout.
     printf(input_string);
