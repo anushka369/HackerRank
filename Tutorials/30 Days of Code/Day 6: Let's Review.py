03ffe5c6d94966dc4eb13f633c33ebec1c79a800
anushka <81908360+anushka369@users.noreply.github.com>
2025-02-15 19:57:41 +0530

    Create Day 6: Let's Review.py

---
+++ Tutorials/30 Days of Code/Day 6: Let's Review.py
@@ -0,0 +1,7 @@
+N = int(input())
+
+for i in range(N):
+    S = input()
+    print(S[::2], S[1::2])
+
+# Link to the problem: https://www.hackerrank.com/challenges/30-review-loop/problem?isFullScreen=true/
