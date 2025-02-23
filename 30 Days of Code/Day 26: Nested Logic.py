205f76b7e0d05e55e585197cd8271046ee5a8f47
anushka <81908360+anushka369@users.noreply.github.com>
2025-02-23 19:35:34 +0530

    Create Day 26: Nested Logic.py

---
+++ 30 Days of Code/Day 26: Nested Logic.py
@@ -0,0 +1,21 @@
+actually = list(map(int, input().split()))
+da, ma, ya = actually
+
+expected = list(map(int, input().split()))
+de, me, ye = expected
+
+fine = 0
+
+if ya > ye:
+    fine = 10000
+  
+elif ya == ye:
+    if ma > me:
+        fine = (ma - me) * 500
+      
+    elif ma == me and da > de:
+        fine = (da - de) * 15
+
+print(fine)
+
+# Link to the problem: https://www.hackerrank.com/challenges/30-nested-logic/problem?isFullScreen=true/
