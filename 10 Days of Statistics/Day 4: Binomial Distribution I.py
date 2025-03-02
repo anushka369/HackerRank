d05d374583aa5247120aa41680cfb658759c8f60
anushka <81908360+anushka369@users.noreply.github.com>
2025-03-02 18:13:53 +0530

    Create Day 4: Binomial Distribution I.py

---
+++ 10 Days of Statistics/Day 4: Binomial Distribution I.py
@@ -0,0 +1,20 @@
+def factorial(n):
+    if n == 1 or n == 0:
+        return 1
+    if n > 1:
+        return factorial(n - 1) * n
+
+def binomial(x, n, p):
+    f = factorial(n) / (factorial(n - x) * factorial(x))
+    return (f * p**x * (1.0 - p)**(n-x))
+
+# Set data
+values = list(map(float, input().split()))
+p = values[0] / (values[0] + values[1])
+n = 6
+
+# Get binomial result
+result = binomial(3,n,p) + binomial(4,n,p) + binomial(5,n,p) + binomial(6,n,p)
+print(round(result, 3))
+
+# Link to the problem: https://www.hackerrank.com/challenges/s10-binomial-distribution-1/problem?isFullScreen=true/
