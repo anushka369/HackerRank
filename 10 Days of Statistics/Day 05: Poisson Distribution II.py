e18559f736c77fdd47a404b29fe217dbc652841b
anushka <81908360+anushka369@users.noreply.github.com>
2025-03-03 09:56:53 +0530

    Create Day 05: Poisson Distribution II.py

---
+++ 10 Days of Statistics/Day 05: Poisson Distribution II.py
@@ -0,0 +1,9 @@
+lambdas = list(map(float, input().split()))
+lambda_a = lambdas[0]
+lambda_b = lambdas[1]
+
+# Gets the result and show on the screen
+print (round(160 + 40 * (lambda_a + lambda_a ** 2), 3))
+print (round(128 + 40 * (lambda_b + lambda_b ** 2), 3))
+
+# Link to the problem: https://www.hackerrank.com/challenges/s10-poisson-distribution-2/problem?isFullScreen=true/
