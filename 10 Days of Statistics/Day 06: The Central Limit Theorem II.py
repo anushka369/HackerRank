2c03fd17c681657fb2a2fac9007c119a8d690566
anushka <81908360+anushka369@users.noreply.github.com>
2025-03-04 22:23:21 +0530

    Create Day 06: The Central Limit Theorem II.py

---
+++ 10 Days of Statistics/Day 06: The Central Limit Theorem II.py
@@ -0,0 +1,20 @@
+# Import library
+import math
+
+# Define functions
+def cumulative(mean, std, value):
+    return 0.5 * (1 + math.erf((value - mean) / (std * (2 ** 0.5))))
+
+# Set data
+max_weight = float(input())
+n = float(input())
+mean = float(input())
+std = float(input())
+
+new_mean = mean * n
+new_std = math.sqrt(n) * std
+
+# Gets the result and show on the screen
+print (round(cumulative(new_mean, new_std, max_weight),4))
+
+# Link to the problem: https://www.hackerrank.com/challenges/s10-the-central-limit-theorem-2/problem?isFullScreen=true/
